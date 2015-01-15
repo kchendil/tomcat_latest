d802c75f336360d9df99c30e1b799a7d12dbb098
Sunggun Yu <sunggun.dev@gmail.com>
2015-01-15 00:03:03 -0500

    Some Enhancement and changes.
    - Change the tomcat home directory name by adding --strip-components=1. for example, {tomcat_install_loc}/tomcat7/apache-tomcat-1 -> {tomcat_install_loc}/tomcat7
     - also, moving directory function has been removed.
    - new tomcat.erb for tomcat script.
    - /etc/rc.d/tomcat(6|7) for fedora or centos has changed to /etc/init.d/tomcat(6|7)
    - chown for tomcat_user has been removed. root will be the owner of init.d script.

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,269 +26,230 @@ tomcat_user=node['tomcat_latest']['tomcat_user']
 auto_start=node['tomcat_latest']['auto_start']
 
 
-if ( !(File.exists?("/etc/init.d/tomcat7") || File.exists?("/etc/init.d/tomcat6") || File.exists?("/etc/init.d/tomcat") || File.exists?("/etc/rc.d/tomcat7") || File.exists?("/etc/rc.d/tomcat6") || File.exists?("/etc/rc.d/tomcat")))
+if !(File.exists?('/etc/init.d/tomcat7') || File.exists?('/etc/init.d/tomcat6') || File.exists?('/etc/init.d/tomcat') || File.exists?('/etc/rc.d/tomcat7') || File.exists?('/etc/rc.d/tomcat6') || File.exists?('/etc/rc.d/tomcat'))
 
-if platform=="suse" || platform=="centos" || platform=="fedora" || platform=="ubuntu" || platform=="debian"
+  if platform=='suse' || platform=='centos' || platform=='fedora' || platform=='ubuntu' || platform=='debian'
 
-if direct_download_version!="na"
+    if direct_download_version!='na'
 
-include_recipe "java"
-if ( direct_download_version =~ /7(.*)/ ) 
-  direct_download_url= "http://archive.apache.org/dist/tomcat/tomcat-7/v"+"#{direct_download_version}"+"/bin/apache-tomcat-#{direct_download_version}.tar.gz";
-else if ( direct_download_version =~ /6(.*)/ )
-   direct_download_url= "http://archive.apache.org/dist/tomcat/tomcat-6/v"+"#{direct_download_version}"+"/bin/apache-tomcat-#{direct_download_version}.tar.gz";
-else 
-	abort("Unsupported tomcat version "+"#{direct_download_version}"+"specified")
-	end
-end
+      include_recipe 'java'
+      if direct_download_version =~ /7(.*)/
+        direct_download_url= 'http://archive.apache.org/dist/tomcat/tomcat-7/v'+"#{direct_download_version}"+"/bin/apache-tomcat-#{direct_download_version}.tar.gz";
+      else
+        if (direct_download_version =~ /6(.*)/)
+          direct_download_url= 'http://archive.apache.org/dist/tomcat/tomcat-6/v'+"#{direct_download_version}"+"/bin/apache-tomcat-#{direct_download_version}.tar.gz";
+        else
+          abort('Unsupported tomcat version '+"#{direct_download_version}"+'specified')
+        end
+      end
 
-script "Download Apache Tomcat #{direct_download_version}" do
-  interpreter "bash"
-  user "#{tomcat_user}"
-  cwd "/tmp"
-  code <<-EOH
-  wget "#{direct_download_url}" -O "/tmp/apache-tomcat-#{direct_download_version}.tar.gz"
-  mkdir -p #{tomcat_install_loc}/tomcat
-  EOH
-end
+      script "Download Apache Tomcat #{direct_download_version}" do
+        interpreter 'bash'
+        cwd '/tmp'
+        code <<-EOH
+          wget "#{direct_download_url}" -O "/tmp/apache-tomcat-#{direct_download_version}.tar.gz"
+          mkdir -p #{tomcat_install_loc}/tomcat
+          chown -R #{tomcat_user} #{tomcat_install_loc}/tomcat
+        EOH
+      end
 
-unzip_tomcat6or7 "d" do
-  enable true
-end
+      unzip_tomcat6or7 'd' do
+        enable true
+      end
 
+      if direct_download_version =~ /7(.*)/
+        template "#{tomcat_install_loc}/tomcat/conf/server.xml" do
+          source 'server7.xml.erb'
+          owner "#{tomcat_user}"
+          mode '0644'
+        end
+      else
+        if direct_download_version =~ /6(.*)/
 
+          template "#{tomcat_install_loc}/tomcat/conf/server.xml" do
+            source 'server6.xml.erb'
+            owner "#{tomcat_user}"
+            mode '0644'
+          end
 
-execute "Change the directory name to apache-tomcat" do
- user "#{tomcat_user}"
- cwd #{tomcat_install_loc}/tomcat
- command "cd #{tomcat_install_loc}/tomcat; mv apache-tomcat-* apache-tomcat"
-  not_if { ::File.directory?("/#{tomcat_install_loc}/tomcat/apache-tomcat")}
-  action :run
-end
+        else
+          abort('Unsupported tomcat version '+"#{direct_download_version}"+'specified')
+        end
+      end
+      if platform=='suse' || platform=='centos' || platform=='fedora'
+        template '/etc/init.d/tomcat' do
+          source 'tomcat.erb'
+          mode '0755'
+        end
+      end
 
-if ( direct_download_version =~ /7(.*)/ ) 
-   
- template "#{tomcat_install_loc}/tomcat/apache-tomcat/conf/server.xml" do
-  source "server7.xml.erb"
-  owner "#{tomcat_user}"
-  mode "0644"  
-end
-else if ( direct_download_version =~ /6(.*)/ )
+      if platform=='ubuntu' || platform=='debian'
+        template '/etc/init.d/tomcat' do
+          source 'tomcat.erb'
+          mode '0755'
+        end
+      end
+      if auto_start=='true'
+        if platform=='suse' || platform=='ubuntu'|| platform=='debian'
 
-template "#{tomcat_install_loc}/tomcat/apache-tomcat/conf/server.xml" do
-  source "server6.xml.erb"
-  owner "#{tomcat_user}"
-  mode "0644"  
-  end
-  
-else 
-	abort("Unsupported tomcat version "+"#{direct_download_version}"+"specified")
-	end
-end
-if platform=="suse" || platform=="centos" || platform=="fedora"
-template "/etc/rc.d/tomcat" do
-  source "tomcat.erb"
-  owner "#{tomcat_user}"
-  mode "0755"  
-end
-end
+          script 'Start tomcat' do
+            interpreter 'bash'
+            user "#{tomcat_user}"
+            cwd '/tmp'
+            code <<-EOH
+              /etc/init.d/tomcat start
+            EOH
+          end
+        end
+        if platform=='centos' || platform=='fedora'
 
-if platform=="ubuntu" || platform=="debian"
-template "/etc/init.d/tomcat" do
-  source "tomcat.erb"
-  owner "#{tomcat_user}"
-  mode "0755"  
-end
-end
-if auto_start=="true"
-if platform=="suse" || platform=="ubuntu"|| platform=="debian"
-
-script "Start tomcat" do
-  interpreter "bash"
-  user "#{tomcat_user}"
-  cwd "/tmp"
-  code <<-EOH  
-  /etc/init.d/tomcat start
-  EOH
-end
-end
-if platform=="centos" || platform=="fedora"
-
-script "Start tomcat" do
-  interpreter "bash"
-  user "#{tomcat_user}"
-  cwd "/tmp"
-  code <<-EOH  
-  /etc/rc.d/tomcat start
-  EOH
-end
-end
-end
-end
-if direct_download_version=="na"
+          script 'Start tomcat' do
+            interpreter 'bash'
+            user "#{tomcat_user}"
+            cwd '/tmp'
+            code <<-EOH
+              /etc/init.d/tomcat start
+            EOH
+          end
+        end
+      end
+    end
+    if direct_download_version=='na'
 
 
-include_recipe "java"
+      include_recipe 'java'
 
 #convert version number to a string if it isn't already
-if tomcat_version.instance_of? Fixnum
-  tomcat_version = tomcat_version.to_s
-end
+      if tomcat_version.instance_of? Fixnum
+        tomcat_version = tomcat_version.to_s
+      end
 
-case tomcat_version
-when "6"
-tomcat_url = node['tomcat_latest']['tomcat_url_6']
-script "Download Apache Tomcat 6 " do
-  interpreter "bash"
-  user "#{tomcat_user}"
-  cwd "/tmp"
-  code <<-EOH
-  wget #{tomcat_url} -O /tmp/tomcat_pag.txt
-  url=`grep -m 1 apache-tomcat-6.*.[0-9][0-9].tar.[g][z] /tmp/tomcat_pag.txt | cut -d '"' -f 2`
-  wget $url 
-  mkdir -p #{tomcat_install_loc}/tomcat6
-  EOH
-end
-
-
-unzip_tomcat6or7 "6" do
-  enable true
-end
-execute "Change the directory name to apache-tomcat-6" do
- user "#{tomcat_user}" 
- cwd #{tomcat_install_loc}/tomcat6
- command "cd #{tomcat_install_loc}/tomcat6; mv apache-tomcat-6.* apache-tomcat-6"
- action :run
-end
+      case tomcat_version
+        when '6'
+          tomcat_url = node['tomcat_latest']['tomcat_url_6']
+          script 'Download Apache Tomcat 6 ' do
+            interpreter 'bash'
+            user "#{tomcat_user}"
+            cwd '/tmp'
+            code <<-EOH
+              wget #{tomcat_url} -O /tmp/tomcat_pag.txt
+              url=`grep -m 1 apache-tomcat-6.*.[0-9][0-9].tar.[g][z] /tmp/tomcat_pag.txt | cut -d '"' -f 2`
+              wget $url
+              mkdir -p #{tomcat_install_loc}/tomcat6
+            EOH
+          end
 
 
-template "#{tomcat_install_loc}/tomcat6/apache-tomcat-6/conf/server.xml" do
-  source "server6.xml.erb"
-  owner "#{tomcat_user}"
-  mode "0644"  
-end
-if platform=="suse" || platform=="centos" || platform=="fedora"
-template6or7 "6" do
-  user true
-end
-end
+          unzip_tomcat6or7 '6' do
+            enable true
+          end
 
-if platform=="ubuntu" || platform=="debian"
-template "/etc/init.d/tomcat6" do
-  source "tomcat6.erb"
-  owner "#{tomcat_user}"
-  mode "0755"  
-end
-end
-if auto_start =="true"
-if platform=="suse" ||platform=="ubuntu" || platform=="debian"
+          template "#{tomcat_install_loc}/tomcat6/conf/server.xml" do
+            source 'server6.xml.erb'
+            owner "#{tomcat_user}"
+            mode '0644'
+          end
+          if platform=='suse' || platform=='centos' || platform=='fedora'
+            template6or7 '6' do
+              user true
+            end
+          end
 
-script "Start tomcat 6 on #{platform}" do
-  interpreter "bash"
-  user "#{tomcat_user}"
-  cwd "/tmp"
-  code <<-EOH  
-  /etc/init.d/tomcat6 start
-  EOH
-end
-end
-if platform=="centos" || platform=="fedora" 
+          if platform=='ubuntu' || platform=='debian'
+            template '/etc/init.d/tomcat6' do
+              source 'tomcat6.erb'
+              owner "#{tomcat_user}"
+              mode '0755'
+            end
+          end
+          if auto_start =='true'
+            if platform=='suse' ||platform=='ubuntu' || platform=='debian'
 
-script "Start tomcat 6 on #{platform}" do
-  interpreter "bash"
-  user "#{tomcat_user}"
-  cwd "/tmp"
-  code <<-EOH  
-  /etc/rc.d/tomcat6 start
-  EOH
-end
-end
-end
+              script "Start tomcat 6 on #{platform}" do
+                interpreter 'bash'
+                user "#{tomcat_user}"
+                cwd '/tmp'
+                code <<-EOH
+                  /etc/init.d/tomcat6 start
+                EOH
+              end
+            end
+            if platform=='centos' || platform=='fedora'
 
-when "7"
-tomcat_url = node['tomcat_latest']['tomcat_url_7']
-script "Download Apache Tomcat 7 " do
-  interpreter "bash"
-  user "#{tomcat_user}"
-  cwd "/tmp"
-  code <<-EOH
-  wget #{tomcat_url} -O /tmp/tomcat_pag.txt
-  url=`grep -m 1 apache-tomcat-7.*.[0-9][0-9].tar.[g][z] /tmp/tomcat_pag.txt | cut -d '"' -f 2`
-  wget $url 
-  mkdir -p #{tomcat_install_loc}/tomcat7
-  EOH
-end
+              script "Start tomcat 6 on #{platform}" do
+                interpreter 'bash'
+                user "#{tomcat_user}"
+                cwd '/tmp'
+                code <<-EOH
+                  /etc/init.d/tomcat6 start
+                EOH
+              end
+            end
+          end
 
-unzip_tomcat6or7 "7" do
-  enable true
-end
+        when '7'
+          tomcat_url = node['tomcat_latest']['tomcat_url_7']
+          script 'Download Apache Tomcat 7 ' do
+            interpreter 'bash'
+            cwd '/tmp'
+            code <<-EOH
+              wget #{tomcat_url} -O /tmp/tomcat_pag.txt
+              url=`grep -m 1 apache-tomcat-7.*.[0-9][0-9].tar.[g][z] /tmp/tomcat_pag.txt | cut -d '"' -f 2`
+              wget $url
+              mkdir -p #{tomcat_install_loc}/tomcat7
+              chown -R #{tomcat_user} #{tomcat_install_loc}/tomcat7
+            EOH
+          end
 
-execute "Change the directory name to apache-tomcat-7" do
- user "#{tomcat_user}"
- cwd #{tomcat_install_loc}/tomcat7
- command "cd #{tomcat_install_loc}/tomcat7; mv apache-tomcat-7.* apache-tomcat-7"
- action :run
-end
+          unzip_tomcat6or7 '7' do
+            enable true
+          end
 
+          template "#{tomcat_install_loc}/tomcat7/conf/server.xml" do
+            source 'server7.xml.erb'
+            owner "#{tomcat_user}"
+            mode '0644'
+          end
+          if platform=='suse' || platform=='centos' || platform=='fedora'
+            template6or7 '7' do
+              user "#{tomcat_user}"
+            end
+          end
 
-template "#{tomcat_install_loc}/tomcat7/apache-tomcat-7/conf/server.xml" do
-  source "server7.xml.erb"
-  owner "#{tomcat_user}" 
-  mode "0644"  
-end
-if platform=="suse" || platform=="centos" || platform=="fedora"
-template6or7 "7" do
-  user "#{tomcat_user}"
-end
-end
+          if platform=='ubuntu'
+            template '/etc/init.d/tomcat7' do
+              source 'tomcat7.erb'
+              mode '0755'
+            end
+          end
 
-if platform=="ubuntu" 
-template "/etc/init.d/tomcat7" do
-  source "tomcat7.erb"
-  owner "#{tomcat_user}"
-  mode "0755"  
-end
-end
-
-if auto_start =="true"
-if platform=="suse" ||platform=="ubuntu" || platform=="debian"
-
-script "Start tomcat 7" do
-  interpreter "bash"
-  user "#{tomcat_user}"
-  cwd "/tmp"
-  code <<-EOH  
-  /etc/init.d/tomcat7 start
-  EOH
-end
-end
-if platform=="centos" || platform == "fedora"
-
-script "Start tomcat 7" do
-  interpreter "bash"
-  user "#{tomcat_user}"
-  cwd "/tmp"
-  code <<-EOH  
-  /etc/rc.d/tomcat7 start
-  EOH
-end
-
-end
-end
-
-end
-
-end
-
+          if auto_start =='true'
+            if platform=='suse' ||platform=='ubuntu' || platform=='debian'
+              script 'Start tomcat 7' do
+                interpreter 'bash'
+                cwd '/tmp'
+                code <<-EOH
+                  /etc/init.d/tomcat7 start
+                EOH
+              end
+            end
+            if platform=='centos' || platform == 'fedora'
+              script 'Start tomcat 7' do
+                interpreter 'bash'
+                cwd '/tmp'
+                code <<-EOH
+                  /etc/init.d/tomcat7 start
+                EOH
+              end
+            end
+          end
+      end
+    end
+  else
+    Chef::Log.info("#{platform} #{platform_version} is not yet supported.")
+  end
 
 else
-
-Chef::Log.info("#{platform} #{platform_version} is not yet supported.")
-
-end
-
-else
-
-Chef::Log.info("tomcat_latest chef cookbook is already installed")
+  Chef::Log.info('tomcat_latest chef cookbook is already installed')
 end
 
